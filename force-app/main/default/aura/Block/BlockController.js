f3ab45b63ed971abe6cefc5d088e146351a9fe19
sannalavamsi <vamsireddysannala@gmail.com>
2026-03-22 16:37:16 +0530

    Aura-handson

---
+++ force-app/main/default/aura/Block/BlockController.js
@@ -0,0 +1,7 @@
+({
+    blockClickHandler : function(component, event, helper) {
+        const open = component.get('v.open');
+        if(open==false)
+        component.set('v.open', true);
+    }
+})
---
+++ force-app/main/default/aura/Board/BoardHelper.js
@@ -0,0 +1,128 @@
+({
+    getWords: function(count) {
+        if(count>100) return;
+        //build an array
+        let wordsArray=[
+        "expansion",
+            "grandfather",
+            "nappy",
+            "oranges",
+            "beds",
+            "quack",
+            "achiever",
+            "yell",
+            "hospital",
+            "winter",
+            "understood",
+            "squalid",
+            "merciful",
+            "reaction",
+            "wipe",
+            "fearless",
+            "tiresome",
+            "introduce",
+            "planes",
+            "drum",
+            "muddle",
+            "capable",
+            "canvas",
+            "route",
+            "enchanted",
+            "quirky",
+            "switch",
+            "apparatus",
+            "loss",
+            "agreement",
+            "substance",
+            "back",
+            "oafish",
+            "expand",
+            "aromatic",
+            "quarrelsome",
+            "free",
+            "useful",
+            "raspy",
+            "drown",
+            "ring",
+            "lush",
+            "numberless",
+            "embarrass",
+            "shrill",
+            "rice",
+            "ice",
+            "crow",
+            "pumped",
+            "sparkle",
+            "instruct",
+            "girl",
+            "glass",
+            "frog",
+            "murky",
+            "impolite",
+            "crabby",
+            "pin",
+            "grade",
+            "upbeat",
+            "linen",
+            "flaky",
+            "side",
+            "unknown",
+            "cactus",
+            "round",
+            "busy",
+            "grab",
+            "crush",
+            "faithful",
+            "mother",
+            "clean",
+            "unhealthy",
+            "event",
+            "absent",
+            "thoughtless",
+            "icy",
+            "prefer",
+            "charge",
+            "confuse",
+            "clam",
+            "dress",
+            "snake",
+            "evasive",
+            "unit",
+            "flow",
+            "annoying",
+            "gusty",
+            "possessive",
+            "rhetorical",
+            "rule",
+            "frantic",
+            "farm",
+            "poor",
+            "possess",
+            "men",
+            "pleasant",
+            "zoom",
+            "sidewalk",
+            "reply"
+        ];
+        //randomize words array
+        wordsArray=this.randmoizeArray(wordsArray);
+        //return requested words
+       return wordsArray.slice(0,count);
+    },
+    randmoizeArray: function(arr) {
+        const randomarr=arr;
+        //randomize an array
+        for (let i = randomarr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [randomarr[i], randomarr[j]] = [arr[j], arr[i]];
+        }
+        return randomarr;
+    },
+     getWinWord: function(arr) {
+        const randomIndex=Math.floor(Math.random() * arr.length);
+        return arr[randomIndex];
+     }
+
+
+
+});
